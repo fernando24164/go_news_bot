a378603b577b65d7b180710156b580e467a36dd2
Jorge Lopez <jorge.lopez@a3sec.com>
2016-12-13 18:03:27 +0100

    Add schedule function for polling process and add getitems function

--- rssparser.go
+++ rssparser.go
@@ -4,9 +4,12 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -54,6 +57,7 @@ func getfeed(url string) []byte {
 	return data
 }
 
+// Save all the items get from the subcriptions on the database
 func dbsaver(feed rss) {
 	db, err := sql.Open("sqlite3", "./feedb.db")
 	if err != nil {
@@ -64,14 +68,14 @@ func dbsaver(feed rss) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	// TABLE structure : create table items(item text, link text, pubdate integer, channel text)
-	stmt, err := tx.Prepare("INSERT INTO items(item, link, pubdate, channel) VALUES(?, ?, ?, ?)")
+	// TABLE structure : create table items(item text, link text, pubdate integer, channel text, sent integer)
+	stmt, err := tx.Prepare("INSERT INTO items(item, link, pubdate, channel, sent) VALUES(?, ?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer stmt.Close()
 	for _, item := range feed.Channel.Items {
-		_, err = stmt.Exec(item.Title, item.Link, item.PubDate, feed.Channel.Title)
+		_, err = stmt.Exec(item.Title, item.Link, item.PubDate, feed.Channel.Title, 0)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -79,10 +83,71 @@ func dbsaver(feed rss) {
 	tx.Commit()
 }
 
+// Get the items from de database with the flag for only not sent ones or everything
+func getitems(onlynew bool) {
+	db, err := sql.Open("sqlite3", "./feedb.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM items")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var item, link, pubdate, channel string
+		var sent int
+		rows.Scan(&item, &link, &pubdate, &channel, &sent)
+		if onlynew {
+			fmt.Println("Item: ", item, " - Link : ", link, " - Publication date : ",
+				pubdate, " - Channel : ", channel)
+		} else {
+			fmt.Println("Item: ", item, " - Link : ", link, " - Publication date : ",
+				pubdate, " - Channel : ", channel, " - Sent : ", sent)
+		}
+	}
+}
+
+// Search for the list of subcriptions for a user
+func getfeedsubcriptions(user string) []string {
+	// TABLE structure : create table subcriptions(id int, name text, list text)
+	// INSERT : INSERT INTO subcriptions(id, name, list) VALUES("1", "agpatag", "http://feeds.weblogssl.com/genbeta,http://www.eldiario.es/rss")
+	db, err := sql.Open("sqlite3", "./feedb.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	var list string
+	err = db.QueryRow("SELECT list FROM subcriptions WHERE name = ?", user).Scan(&list)
+	if err != nil {
+		log.Fatal(err)
+	}
+	result := make([]string, 0)
+	for _, source := range strings.Split(list, ",") {
+		result = append(result, source)
+	}
+	return result
+}
+
+// Sleep a search for the feed
+func scheduler(sleeper int, user string) {
+	fmt.Println(" # Starting the schedule . . .")
+	for {
+		fmt.Println(" > Repolling Subcriptions!")
+		subcriptions := getfeedsubcriptions(user)
+		for _, url := range subcriptions {
+			data := getfeed(url)
+			feed := feedreader(data)
+			dbsaver(feed)
+		}
+		time.Sleep(time.Duration(sleeper) * time.Second)
+	}
+}
+
 func main() {
-	//url := "http://feeds.weblogssl.com/genbeta"
-	url := "http://www.eldiario.es/rss/"
-	data := getfeed(url)
-	feed := feedreader(data)
-	dbsaver(feed)
+	sleeper := 60
+	user := "agpatag"
+	scheduler(sleeper, user)
+	// getitems(false)
+	// urls := getfeedsubcriptions(user)
 }
