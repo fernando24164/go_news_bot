f0eb2cc90377baa4e26ebf7adee864584f999d7b
Jorge Lopez <jorge.lopez@a3sec.com>
2016-12-20 00:14:24 +0100

    Modify gettitems to return a json object

--- apiserver.go
+++ apiserver.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,28 +9,43 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+type jList struct {
+	items []jItem
+}
+
+type jItem struct {
+	Title   string
+	Link    string
+	PubDate string
+}
+
 // Get the items from de database with the flag for only not sent ones or everything
-func getitems(onlynew bool) string {
+func getitems(onlynew bool) []byte {
 	rows, err := db.Query("SELECT * FROM items")
 	defer rows.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
-	var response string
+	var responseList []jItem
 	for rows.Next() {
 		var item, link, pubdate, channel string
 		var sent int
 		rows.Scan(&item, &link, &pubdate, &channel, &sent)
-		response = response + "Item: " + item + " - Link : " + link + " - Publication date : " + pubdate + " - Channel : " + channel + "&&"
+		responseList = append(responseList, jItem{item, link, pubdate})
 	}
-	return response
+	response := jList{responseList}
+	jsonResp, err := json.Marshal(response)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return jsonResp
 }
 
 func itemsresponse(w http.ResponseWriter, r *http.Request) {
 	//user := r.URL.Path[len("/getfeed/"):]
 	//fmt.Println(user)
 	//getitems(false)
-	fmt.Fprintf(w, getitems(false), "")
+	fmt.Println(getitems(false))
 }
 
 // API function to query the database
